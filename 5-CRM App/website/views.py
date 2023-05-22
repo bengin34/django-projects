0c11e9c3b9f0414a299fa113c91a955a640db34e
bengin34 <becaglar3434@gmail.com>
2023-05-22 10:51:57 +0300

    customer record

--- 5-CRM App/website/views.py
+++ 5-CRM App/website/views.py
@@ -25,7 +25,7 @@ def home(request):
             messages.success(request, "There was an error logging in, Please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 
@@ -51,3 +51,13 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
     
     return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
