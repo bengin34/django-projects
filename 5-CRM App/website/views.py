c9fe486c6a2e72551f860fdbb607b3bb1d51345e
bengin34 <becaglar3434@gmail.com>
2023-05-22 10:18:50 +0300

    admin

--- 5-CRM App/website/views.py
+++ 5-CRM App/website/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
-
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
+
+
+
+
     #check to see if logged in
     if request.method == 'POST':
         username = request.POST['username']
@@ -29,4 +35,19 @@ def logout_user(request):
         return redirect('home')
 
 def register_user(request):
-       return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password = password)
+            login(request, user)
+            messages.success(request, 'You have successfully Register')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
